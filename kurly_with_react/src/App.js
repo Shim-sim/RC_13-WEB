e747a94c16a85b539f008bef96e8adf76aa8f4a4
ShimSungBo <shimdb96@gmail.com>
2022-12-26 20:04:38 +0900

    feat:(kurly) 매인베너(carousel)완성

--- kurly_with_react/src/App.js
+++ kurly_with_react/src/App.js
@@ -3,12 +3,14 @@
 import Header from './components/Common/Header';
 import NavBar from './components/Common/NavBar';
 import styled from 'styled-components';
+import Banner from './Banner';
 
 function App() {
   return (
     <>
       <Header />
       <NavBar />
+      <Banner />
     </>
   );
 }
---
+++ kurly_with_react/src/Banner.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+import Carousel from 'react-bootstrap/Carousel';
+
+function Banner() {
+  return (
+    <Carousel>
+      {[0, 1, 2, 3].map((item, index) => (
+        <Carousel.Item key={item} interval={2000}>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + 'banner' + `${item}.jpeg`}
+            alt="First slide"
+            style={{ height: '370px' }}
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
+
+export default Banner;
