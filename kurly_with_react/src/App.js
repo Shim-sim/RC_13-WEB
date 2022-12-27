c931320bcd27d592e01360918f4b8596466c65ac
ShimSungBo <shimdb96@gmail.com>
2022-12-27 11:18:41 +0900

    refactor:(kurly) react-router-dom을 사용한 페이지 분리

--- kurly_with_react/src/App.js
+++ kurly_with_react/src/App.js
@@ -3,14 +3,23 @@
 import Header from './components/Common/Header';
 import NavBar from './components/Common/NavBar';
 import styled from 'styled-components';
-import Banner from './Banner';
+import Main from './pages/Main';
+import Detail from './pages/Detail';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import { Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
       <NavBar />
-      <Banner />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
