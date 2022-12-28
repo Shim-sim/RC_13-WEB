89fe5d2019e74c3959172bfc8718da291d572fed
ShimSungBo <shimdb96@gmail.com>
2022-12-28 21:37:09 +0900

    chore: reset 추가

--- kurly_with_react/src/App.js
+++ kurly_with_react/src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-
+import { Reset } from 'styled-reset';
 import Header from './components/Common/Header';
 import NavBar from './components/Common/NavBar';
 import styled from 'styled-components';
@@ -7,11 +7,13 @@ import Main from './pages/Main';
 import Detail from './pages/Detail';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
+import Footer from './components/Common/Footer';
 import { Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <Reset />
       <Header />
       <NavBar />
       <Routes>
@@ -20,6 +22,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      <Footer />
     </>
   );
 }
