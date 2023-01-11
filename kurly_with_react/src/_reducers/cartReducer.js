c3a9d493f9c626be4b64bfeca389e4410f47202b
ShimSungBo <shimdb96@gmail.com>
2023-01-11 16:09:34 +0900

    feat: cartReducer 추가

--- kurly_with_react/src/_reducers/cartReducer.js
+++ kurly_with_react/src/_reducers/cartReducer.js
@@ -1,12 +1,31 @@
 const initailState = {
   cart: [],
+  count: 1,
   total: 0,
 };
 
 const cartReducer = (state = initailState, action) => {
   switch (action.type) {
-    case 'ADD_ITEM':
-      return [...state, action.payload];
+    case 'ADD_CART':
+      const cartItem = state.cart.find((item) => item.id === action.payload.id);
+      if (cartItem) {
+        cartItem.quantity += action.payload.quantity;
+      } else {
+        const addToCart = {
+          id: action.payload.id,
+          title: action.payload.title,
+          price: action.payload.price,
+          img: action.payload.img,
+          quantity: action.payload.quantity,
+        };
+        state.cart.push(addToCart);
+        console.log(state.cart);
+      }
+
+      return {
+        ...state,
+        cart: [...state.cart],
+      };
 
     case 'CONVERT':
       return action.payload.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
