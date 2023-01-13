ce40eea324cb7cd16d3e0cd6f56fa52b8accc808
ShimSungBo <shimdb96@gmail.com>
2023-01-13 16:37:47 +0900

    style: navBar 반응형 추가

--- kurly_with_react/src/components/Common/NavStyle/List.js
+++ kurly_with_react/src/components/Common/NavStyle/List.js
@@ -22,17 +22,43 @@ const Wrapper = styled.ul`
     text-decoration: underline;
     text-underline-position: under;
   }
-}
+  @media screen and (max-width: 890px) {
+    display: none;
+  }
+`;
+
+const StyledInput = styled.div`
+  background-color: rgb(247, 247, 247);
+  height: 36px;
+  display: none;
+  border-radius: 10px;
+
+  input {
+    width: 230px;
+    font-size: 13px;
+    background-color: rgb(247, 247, 247);
+    padding: 8px;
+    border-radius: 8px;
+  }
+
+  @media screen and (max-width: 890px) {
+    display: flex;
+  }
 `;
 
 const List = () => {
   return (
-    <Wrapper>
-      <li>신상품</li>
-      <li>베스트</li>
-      <li>알뜰쇼핑</li>
-      <li>특가/혜택</li>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <li>신상품</li>
+        <li>베스트</li>
+        <li>알뜰쇼핑</li>
+        <li>특가/혜택</li>
+      </Wrapper>
+      <StyledInput>
+        <input placeholder="검색어를 입력해주세요" />
+      </StyledInput>
+    </>
   );
 };
 
